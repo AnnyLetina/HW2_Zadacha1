74691f45d30f5860e058396bb19cc101255de49e
Anna Letina <letina92@ya.ru>
2023-06-26 18:58:20 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 1001; // стоимость билета
+        int bonus = price / 20; // рубли для бонусной мили
+
+        System.out.println("Количество бонусных миль за покупку билета:");
+        System.out.println((bonus) + " бонусных(ые) мили(ль)");
+
+    }
+}
